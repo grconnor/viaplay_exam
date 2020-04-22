b6ba2acbf0d32e1d041c37b310517cec3e694ec4
emtalen <59577366+emtalen@users.noreply.github.com>
2020-04-22 16:13:08 +0200

    creates function to fetch the data and display pictures of the series

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSeries } from "./modules/seriesAction";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const seriesCollection = useSelector((state) => state.seriesCollection);
+  useEffect(() => {
+    fetchSeries(dispatch);
+  }, []);
 
-export default App
+  let displaySeries;
+  if (seriesCollection) {
+    displaySeries = seriesCollection.map((serie) => {
+      return (
+        <img
+          src={serie.content.images.boxart.url}
+          alt={serie.content.originalTitle}
+        />
+      );
+    });
+  }
+
+  return <div>{displaySeries}</div>;
+};
+
+export default App;
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,8 +1,16 @@
-import initialState from '../store/initialState'
-import * as actionTypes from '../actions/actionTypes'
+import initialState from "../store/initialState";
+import * as actionTypes from "../actions/actionTypes";
 
-const rootReducer = (state = initialState, actions) => {
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.GET_SERIES:
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
-}
-
-export default rootReducer
+export default rootReducer;
