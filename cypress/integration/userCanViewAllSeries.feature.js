5689a7f3b115ba21fd2b4bbe5ff9adeb7ad2cae0
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-10-21 22:34:56 +0200

    Formats integration test file.

--- cypress/integration/userCanViewAllSeries.feature.js
+++ cypress/integration/userCanViewAllSeries.feature.js
@@ -7,11 +7,11 @@ describe("Visitor can see interface", () => {
         "https://cors-anywhere.herokuapp.com/https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga",
       response: "fixture:viaplay_series.json",
     });
-    cy.visit("http://localhost:3000")
-  })
+    cy.visit("http://localhost:3000");
+  });
 
   it("lists the viaplay series", () => {
-    cy.get("[data-cy='header']").should("be.visible")
-    cy.get(".display-show").should("have.length", 10)
+    cy.get("[data-cy='header']").should("be.visible");
+    cy.get(".display-show").should("have.length", 10);
   });
-})
+});
