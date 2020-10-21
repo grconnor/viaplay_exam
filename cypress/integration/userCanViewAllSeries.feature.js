46415a538a0d75694e0ced115da0b11a9f34b117
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-10-21 21:47:06 +0200

    Adds vscode to gitignore file, Creates fixture file for the returned response from viaplay/postman. Writes first test to check that all series are displayed.

---
+++ cypress/integration/userCanViewAllSeries.feature.js
@@ -0,0 +1,17 @@
+describe("Visitor can see interface", () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url:
+        "https://cors-anywhere.herokuapp.com/https://cors-anywhere.herokuapp.com/https://content.viaplay.se/pc-se/serier/samtliga",
+      response: "fixture:viaplay_series.json",
+    ));
+    cy.visit("http://localhost:3000")
+  })
+
+  it("lists the viaplay series", () => {
+    cy.get("[data-cy='header']").should("be.visible")
+    cy.get(".display-show").should("have.length", 10)
+  });
+})
